7283dbef14c5b94544152373a10b2a17546d2872
Ivar Nesje <ivarne@gmail.com>
2018-11-10 15:55:40 +0100

    Update dependencies  (Because github complains about security risk)

---
+++ build/precache-manifest.145a45cc8fbbb5b4438d1c537eef8139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "312c480d3cf37ca314ed",
+    "url": "/vectorracer/build/static/css/main.dbcecbb1.chunk.css"
+  },
+  {
+    "revision": "312c480d3cf37ca314ed",
+    "url": "/vectorracer/build/static/js/main.312c480d.chunk.js"
+  },
+  {
+    "revision": "a37c771a3328447bc2f9",
+    "url": "/vectorracer/build/static/js/1.a37c771a.chunk.js"
+  },
+  {
+    "revision": "180b8b39dec81ee7e909",
+    "url": "/vectorracer/build/static/js/runtime~main.180b8b39.js"
+  },
+  {
+    "revision": "cc3b13a306ed651f3e2fa2ab39bd8793",
+    "url": "/vectorracer/build/index.html"
+  }
+];
